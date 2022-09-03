535c5c6373debdf3f7e4bb79f24a3c41841f25d4
Bartosz Wzorek <wzorek83@gmail.com>
2022-09-03 21:27:36 +0100

    Add files via upload
    added javascript code - but it doesn't work as it should (or as shown on yt video;)) most probably there's type-o in one of the more complicated functions. 
    //note to myself - great exercies, but it's difficult to follow mainly because I don't know all the functions and the whole code is not always visible;/
    /

---
+++ script.js
@@ -0,0 +1,37 @@
+let todos = JSON.parse(localStorage.getItem("myTodos"));
+
+function saver(todosArray) {
+  localStorage.setItem("myTodos", JSON.stringify(todosArray));
+}
+
+function addTOdo() {
+  let inp = document.getElementById("inp").value;
+  if (!inp) {
+    return alert("Please enter todo");
+  }
+  todos.push({ todo: inp, done: false });
+  saver(todos);
+  window.location.reload();
+}
+
+function markDone(id) {
+  todos[id].done === false ? (todos[id].done = true) : (todos[id].done = false);
+  saver(todos);
+  window.location.reload();
+}
+
+function deleteTodo(id){
+    let confirm = window.confirm("Are You sure you want to delete?");
+    if(!confirm) return 
+    todos.splice(id.1);
+    saver(todos)
+    window.location.reload()
+}
+
+document.getElementById('content').innerHTML = todos.map((item,i)=>(
+    `<div class="todo">
+    <div class="inp over" ${item.done === true && 'line'}">${item.todo}</div>
+    <button onclick="markDone(${i})" class="done-btn">${item.done === false ? '<i class="fa-solid fa-check"></i>' : '<i class="fa-solid fa-rotate-right"></i>'} </button>
+    <button onclick="deleteTodo(${i})" class="delete-btn"><i class="fa-solid fa-trash"></i></button>
+    </div>'
+))
